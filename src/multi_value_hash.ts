dd43d4da5ac2d03550da0f1f072d92efb24b3a6e
MOZGIII <mike-n@narod.ru>
2016-02-14 22:00:51 +0300

    Implemented generic MutiValueHash

---
+++ src/multi_value_hash.ts
@@ -0,0 +1,60 @@
+type StringualDictionary<T> = { [key: string]: T }
+type NumericDictionary<T> = { [key: number]: T }
+
+export class MutiValueHash<KeyType, ValueType> {
+  dictionary: StringualDictionary<Array<ValueType>> | NumericDictionary<Array<ValueType>> = {};
+
+  add(key: KeyType, value: ValueType) {
+    var values = this.dictionary[<any>key];
+    if (typeof values === "undefined") {
+      values = [];
+      this.dictionary[<any>key] = values;
+    }
+    values.push(value);
+  }
+
+  remove(key: KeyType, value: ValueType) {
+    var values = this.dictionary[<any>key];
+    if (typeof values === "undefined") {
+      return;
+    }
+    var i = values.length;
+    while (i) {
+      if (values[i] === value) {
+        values.splice(i, 1);
+      } else {
+        i--;
+      }
+    }
+    if (values.length == 0) {
+      this.flush(key);
+    }
+  }
+
+  flush(key: KeyType) {
+    delete this.dictionary[<any>key];
+  }
+
+  exist(key: KeyType): boolean {
+    var values = this.dictionary[<any>key];
+    return typeof values === "undefined";
+  }
+
+  valueExist(key: KeyType, value: ValueType): boolean {
+    var values = this.dictionary[<any>key];
+    if (typeof values === "undefined") {
+      return false;
+    }
+
+    if (values.indexOf(value) >= 0) {
+      return true;
+    }
+
+    return false;
+  }
+
+  get(key: KeyType): Array<ValueType> {
+    var values = this.dictionary[<any>key];
+    return (typeof values === "undefined") ? [] : values;
+  }
+}
